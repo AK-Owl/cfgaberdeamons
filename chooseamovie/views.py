426f099e2c181a5f98d64eb570def154586740e5
I-Georg <47190936+I-Georg@users.noreply.github.com>
2019-11-16 14:42:31 +0000

    working but not showing

--- chooseamovie/views.py
+++ chooseamovie/views.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+#from __future__ import unicode_literals
+import requests
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    url = 'https://movie-database-imdb-alternative.p.rapidapi.com/'
+    querystring = {"page":"1","r":"json","s":"Avengers Endgame"}
+
+    headers = {
+    'x-rapidapi-host': "movie-database-imdb-alternative.p.rapidapi.com",
+    'x-rapidapi-key': "9cb9082d80msh1114acf15ea6021p1b870bjsn9ceb55c5624e"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    print(response.text)
+
     return render(request, 'chooseamovie/chooseamovie.html')

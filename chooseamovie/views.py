618f0f66647b10e621499683ac56673ed35b628d
I-Georg <47190936+I-Georg@users.noreply.github.com>
2019-11-20 15:58:23 +0000

    working api

--- chooseamovie/views.py
+++ chooseamovie/views.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
 #from __future__ import unicode_literals
 import requests
+import json
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    url = 'https://movie-database-imdb-alternative.p.rapidapi.com/'
-    querystring = {"page":"1","r":"json","s":"Avengers Endgame"}
 
-    headers = {
-    'x-rapidapi-host': "movie-database-imdb-alternative.p.rapidapi.com",
-    'x-rapidapi-key': "9cb9082d80msh1114acf15ea6021p1b870bjsn9ceb55c5624e"
-    }
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    print(response.text)
+    r = requests.get('http://api.open-notify.org/astros.json')
+    global data
+    data = r.json()
+    print(data)
+    print ("****************************************************")
+    print(data['people'])
+    print ("***********************$$$$$$$$$$$$$$*****************************")
+    #global data1 = data['people'][1]
+    #datax = data[u'people'[u'name']]
+    return render(request, 'chooseamovie/chooseamovie.html',{
+    'people': data['people']
+    })
 
-    return render(request, 'chooseamovie/chooseamovie.html')
+    

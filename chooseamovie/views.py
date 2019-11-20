fd42bda08269dff643999a1736df8f4dac948dbc
AliceOwl <a.kvaratskheliia.18@aberdeen.ac.uk>
2019-11-20 20:59:54 +0000

    merging

--- chooseamovie/views.py
+++ chooseamovie/views.py
@@ -20,4 +20,14 @@ def index(request):
     'people': data['people']
     })
 
-    
+#def subscribe(request):
+#    sub = forms.Subscribe()
+ #   if request.method == 'POST':
+  #      sub = forms.Subscribe(request.POST)
+   #     subject = 'Welcome to DataFlair'
+    #    message = 'Hope you are enjoying your Django Tutorials'
+    #    recepient = str(sub['Email'].value())
+     #   send_mail(subject, 
+     #       message, EMAIL_HOST_USER, [recepient], fail_silently = False)
+     #   return render(request, 'chooseamovie/home.html', {'recepient': recepient})
+    #return render(request, 'chooseamovie/home.html', {'form':sub})
